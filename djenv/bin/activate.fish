3cecca46e1d0fc52e8953c69e3747ae953280bdd
LakeSuburbia <sandermangelschots@telenet.be>
2021-04-27 20:06:50 +0200

    Startup Django fixed - hello world

--- djenv/bin/activate.fish
+++ djenv/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/pi/digitalmeter/djenv"
+set -gx VIRTUAL_ENV "/home/pi/Digital-meter/djenv"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
