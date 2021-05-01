44cd4b959439eee230217de89e7c0e3236004d9c
LakeSuburbia <sandermangelschots@telenet.be>
2021-05-01 13:35:07 +0200

    restapi works but is not safe yet

--- interface/meterapp/views.py
+++ interface/meterapp/views.py
@@ -1,6 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework import viewsets, permissions
+from .models import *
+from .serializers import *
 
 
 def index(request):
     return HttpResponse("Hello, world.")
+
+
+class UsageViewSet(viewsets.ModelViewSet):
+    """
+    API endpoint that allows users to be viewed or edited.
+    """
+    queryset = Usage.objects.all().order_by('-timestamp')
+    serializer_class = UsageSerializer
+    #permission_classes = [permissions.IsAuthenticated]
