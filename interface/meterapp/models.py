a0369ad134a56d8fda9d9ba4794c4926f51c170f
LakeSuburbia <sandermangelschots@telenet.be>
2021-05-02 19:05:17 +0200

    No users

--- interface/meterapp/models.py
+++ interface/meterapp/models.py
@@ -1,9 +1,4 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-# Create your models here.
-class User(AbstractUser):
-    pass
 
 class Usage(models.Model):
     timestamp = models.DateTimeField(auto_now_add=False)
