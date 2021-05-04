cd0f258a258c5af4e99f65a5700ada6172320c4d
LakeSuburbia <sandermangelschots@telenet.be>
2021-05-04 13:41:08 +0200

    Cleanup local

--- local/p1uitlezen.py
+++ local/p1uitlezen.py
@@ -5,18 +5,18 @@ from datetime import datetime
 import requests
 
 
-print ("SAGEMCOM S211")
+print ("Connecting with SAGEMCOM S211")
 print ("Control-C to quit")
 
 
-###################################################################
-## Every second, the SAGEMCOM meter sends serial data.           ##
-## Every minute, we collect this data and store it temporarily.  ##
-## Every hour, we will update our webserver through an API call. ##
-###################################################################
+#####################################################################
+## Every second, the SAGEMCOM meter sends serial data.             ##
+## Every minute, we collect this data and store it temporarily.    ##
+## Every minute, we will update our webserver through an API call. ##
+#####################################################################
 
 
-# The address where we will collect our data through an hourly API call
+# The address where we will collect our data through API calls, each {{frequency_API}} minute
 APIaddress = "http://165.227.215.102:8000/restapi/usage/"
 AuthToken = "ddcbb3fb935828019e3afd39c923a4a40d6f5792"
 # Length of Sagemcom data
@@ -68,7 +68,7 @@ def read_data():
     usage["timestamp"]=datetime.now().strftime("%Y-%m-%dT%H:%M:%S")
     #Store the first iteration
     for i in range(datalength):
-        #Read 1 line of the serial port
+        # Read 1 line of the serial port
         try:
             rawdata = str(ser.readline())
         except:
@@ -77,31 +77,32 @@ def read_data():
         startindex = rawdata.find("(")+1
         stopindex = rawdata.find("*")
 
+        # Get the daytime usage
         if rawdata.find("1.8.1") != -1:
             if startindex != -1 and stopindex != -1:
                 usage["daytime"]=(rawdata[startindex:stopindex])
         
+        # Get the nighttime usage
         if rawdata.find("1.8.2") != -1:
             if startindex != -1 and stopindex != -1:
                 usage["nighttime"]=(rawdata[startindex:stopindex])
 
-    #Wait and monitor data
+    # Wait and monitor data
     for i in range(frequency_DATA-1):
         buffer_data()
 
     return(usage)
 
 
-
+# Send a post request to the REST API
 def store_data():
-    for i in range(frequency_API):
-        newdata = read_data()
-        print(newdata)
-        try:
-            postreq = requests.post(APIaddress, data = newdata, headers={'Authorization': 'token {}'.format(AuthToken)})
-            print (postreq)
-        except:
-            print("Couldn't reach the server.")
+    newdata = read_data()
+    print(newdata)
+    try:
+        postreq = requests.post(APIaddress, data = newdata, headers={'Authorization': 'token {}'.format(AuthToken)})
+        print (postreq)
+    except:
+        print("Couldn't reach the server.")
         
 
 while(1):
